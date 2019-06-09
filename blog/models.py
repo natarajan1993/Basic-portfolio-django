8c2f12a3bd43fcfd0ec40e02341438f1df1c925f
natarajan1993 <natraj_93@yahoo.com>
2019-06-09 12:00:08 -0500

    Added jobs on homepage using bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog model
+# Title
+# Publication Date
+# Text
+# Image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Add blog app to settings
+
+# Create a migration
+
+# Migrate
+
+# Add to admin
